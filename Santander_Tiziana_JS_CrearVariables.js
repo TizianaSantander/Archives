b9f3f789936bee57d1eab978e7f261b601e7d788
TizianaSantander <114449004+TizianaSantander@users.noreply.github.com>
2022-09-26 23:22:46 -0300

    Add files via upload

---
+++ Santander_Tiziana_JS_CrearVariables.js
@@ -0,0 +1,9 @@
+Santander_Tiziana_JS_CrearVariables
+
+Algoritmo
+
+var EdadNec= >=10 
+var AlturaNec= >1 
+var AlturaNec= Metros
+var EdadNec= Años
+String= { " Para que sea posible subirse a la atracción las personas deben tener una altura superior a 1 metro (var AlturaNec) y una edad de 10 años o más (var EdadNec)}
